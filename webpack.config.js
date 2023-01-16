cfbae6070f84c7bb4d568448a6bece05e2c9a6ce
JhordyMora <jhordy.mora@web.de>
2023-01-16 19:15:11 +0100

    First config webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname,"dist"),
+        filename: "bundle.js",
+    },
+    mode: "development",
+    resolve: {
+        extensions: [".js", "jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+                
+            },
+            {
+                test: /\.html$/,
+                use: "html-loader"
+            },
+            {
+                test: /\.(scss|css)$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader",
+				],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                // type: 'assets',
+                use: [
+                    {
+                        loader: 'file-loader',//['svg-url-loader','file-loader'],
+                        options: {
+                            limit: 10000,
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new 
+    ]
+}
