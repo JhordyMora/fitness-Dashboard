9e1e6cb9616a0ce4a046e48a75095b1054c63b40
JhordyMora <jhordy.mora@web.de>
2023-01-19 21:25:37 +0100

    created pages folder and their components.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Dashboard from '../container/Dashboard';
+import Profil from "../container/Profil";
+import Fitness from '../container/Fitness';
+
+const Home = () => {
+    return (
+        <Fitness>
+            <Dashboard>
+
+            </Dashboard>
+            <Profil>
+                
+            </Profil>
+        </Fitness>
+    )
+}
+
+export default Home
---
+++ src/routes/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import NotFound from '../pages/NotFound';
+import "../styles/global.scss";
+
+const App = () => {
+    return (
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="*" element={<NotFound/>} />
+        </Routes>
+    </BrowserRouter>
+    )
+}
+
+export default App
