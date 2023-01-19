fb46cbbf9cd6c9b8b9d43b7ff0c16a42468afebb
JhordyMora <jhordy.mora@web.de>
2023-01-19 22:44:02 +0100

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -39,9 +39,9 @@ module.exports = {
                 use: [
                     {
                         loader: 'file-loader',//['svg-url-loader','file-loader'],
-                        // options: {
-                        //     limit: 10000,
-                        // }
+                        options: {
+                            limit: 10000,
+                        }
                     }
                 ]
             }
